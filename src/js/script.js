05c3e447377d5122a96302410e8dc696e42b1d51
DmitriiTimofeev97 <DmitriiTimofeev97@gmail.com>
2025-07-02 13:00:43 +0300

    Some final changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+//Скрипт навигационного меню
+
 let navItems = document.querySelectorAll(".nav__link_hamburger");
 const features = document.querySelector("#features");
 const bannerImg = document.querySelector("#banner-img");
@@ -12,4 +14,23 @@ item.addEventListener('click', function(){
         navToggle.checked = true;
     }
 });
+});
+
+//Скрипт кнопки прокрутки к началу страницы
+
+const scrollToTopBtn = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 200) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
 });
